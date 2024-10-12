8f8d4e533f82441a9780abd6cee5d2482788c453
nschubi <dev@schulzebilk.de>
2024-10-12 20:25:38 +0200

    add lsp support for html/css

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,5 @@
-return { 
-  "catppuccin/nvim", 
+return {
+  "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html" },
+          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "css" },
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = true },
         })
     end
  }
