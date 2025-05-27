c919257cd4ea0b0105b33fed0d00fda5e8140f33
nschubi <dev@schulzebilk.de>
2025-05-27 20:36:15 +0200

    change hotkey for code action

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -40,7 +40,7 @@ return {
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set({ "n", "v" }, "<leader>rf", vim.lsp.buf.code_action, {})
     end,
   },
 }
