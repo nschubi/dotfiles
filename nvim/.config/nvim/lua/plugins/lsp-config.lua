c7d83c3267fa498f4c71bbb94fb51121ee8f2319
nschubi <dev@schulzebilk.de>
2025-06-09 19:10:34 +0200

    update lsp-config for texlab

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -50,6 +50,9 @@ return {
           }
         }
       })
+      lspconfig.texlab.setup({
+        capabilities = capabilities,
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
