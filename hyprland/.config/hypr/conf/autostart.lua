142de8ad553807e338f1163444e8ae5c8f391139
nschubi <dev@schulzebilk.de>
2026-06-08 21:23:16 +0200

    add idle monitor function

--- hyprland/.config/hypr/conf/autostart.lua
+++ hyprland/.config/hypr/conf/autostart.lua
@@ -8,7 +8,9 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function ()
-   hl.exec_cmd("protonmail-bridge &")
-   hl.exec_cmd("waybar & hyprpaper")
+   hl.exec_cmd("protonmail-bridge")
+   hl.exec_cmd("nextcloud")
+   hl.exec_cmd("waybar")
+   hl.exec_cmd("hyprpaper")
 end)
 
--- hyprland/.config/hypr/conf/monitors.lua
+++ hyprland/.config/hypr/conf/monitors.lua
@@ -12,10 +12,30 @@ hl.monitor({
 })
 
 -- Framework Display
-hl.monitor({
+local laptopMonitor = {
     output   = "eDP-1",
     mode     = "2256x1504@60",
     position = "3440x0",
-    scale    = "1.57",
-})
+    scale    = "1.177",
+    disabled = false,
+}
 
+-- register monitor settings on startup
+hl.monitor(laptopMonitor)
+
+function disableLaptopMonitor()
+    hl.monitor({
+        output = laptopMonitor.output,
+        disabled = true,
+    })
+end
+
+function enableLaptopMonitor()
+    hl.monitor(laptopMonitor)
+    os.execute("hyprctl reload")
+end
+
+-- close lid
+hl.bind("switch:on:Lid Switch", disableLaptopMonitor, { locked = true })
+-- open lid
+hl.bind("switch:off:Lid Switch", enableLaptopMonitor, { locked = true })
